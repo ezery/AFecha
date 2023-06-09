12864e449c8a0adc4b9c1df5f29fe3159cc26bd7
ezery <127971913+ezery@users.noreply.github.com>
2023-06-09 00:18:32 -0300

    Principal.java

--- src/consola/Principal.java
+++ src/consola/Principal.java
@@ -1,14 +1,10 @@
 package consola;
 
-import java.util.Scanner;
-
 import logica.*;
 
 public class Principal {
 
 	public static void main(String[] args) {
-
-		Scanner input = new Scanner(System.in);
 
 		Fecha f = new Fecha(17,4,2008);
 		Alumno a = new Alumno("Gomez","Segundo",100,f);
